ec569e7a8c0171de365de1006e13daf2635b83c8
Prawito <prawito.id@gmail.com>
2025-06-20 16:20:16 +0700

    enhance react router dom use v6

--- src/config/RouterSite.js
+++ src/config/RouterSite.js
@@ -1,16 +1,16 @@
-import { Route, Switch } from "react-router-dom";
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "../pages/Home";
 import NotFound from "../pages/404";
 
 const RoutesSite = () => {
-    return (
-        <Switch>
-            <Route exact path="/" component={Home} />
-            <Route component={NotFound} />
-        </Switch>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 };
 
 export default RoutesSite;
